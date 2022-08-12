9b4f0f63900e74728dc71511fbb699a430c03b46
Makkena <saradhi.Makkena@cdk.com>
2022-08-12 18:03:22 +0530

    updated table1

--- dbscripts/Tables/Table1.sql
+++ dbscripts/Tables/Table1.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset saradhi.makkena:1
-create table demo1.student (
+create table hackschema.student (
     name varchar(50),
     address varchar(50),
     city varchar(30)
