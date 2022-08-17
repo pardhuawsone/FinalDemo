7b094f7cef683912cc7bf6a7608a4291a733449f
Makkena <saradhi.Makkena@cdk.com>
2022-08-17 10:52:43 +0530

    updated table6

--- dbscripts/Tables/Table6.sql
+++ dbscripts/Tables/Table6.sql
@@ -6,3 +6,10 @@ create table hackschema.Emp6 (
     address varchar(50),
     city varchar(30)
 )
+
+--changeset saradhi.makkena:7
+create table hackschema.Emp7 (
+    name varchar(50),
+    address varchar(50),
+    city varchar(30)
+)
