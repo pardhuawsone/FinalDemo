69accaa3938a5378943de51fd6b2d3906c58e700
Makkena <saradhi.Makkena@cdk.com>
2022-08-26 17:04:03 +0530

    added all deployments files

--- dbscripts/Tables/Table2.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
---changeset saradhi.makkena:1
-create table hackschema.Emp2 (
-    name varchar(50),
-    address varchar(50),
-    city varchar(30)
-)
--- dbscripts/Tables/Table5.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
---changeset saradhi.makkena:5
-create table hackschema.Emp5 (
-    name varchar(50),
-    address varchar(50),
-    city varchar(30)
-)
