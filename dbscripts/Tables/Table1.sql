7bc4827a7d27244b1c44c06345a153ae666b5605
pardhuawsone <pardhuawsone@gmail.com>
2022-08-26 18:13:19 +0530

    Update Table1.sql

--- dbscripts/Tables/Table1.sql
+++ dbscripts/Tables/Table1.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset saradhi.makkena:1
-create table hackschema.Emp1 (
+create table demo.Emp1 (
     name varchar(50),
     address varchar(50),
     city varchar(30)
