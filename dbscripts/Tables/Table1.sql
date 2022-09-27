a09276c89485ebc79ac4203928f6b8cc882a6d57
pardhuawsone <pardhuawsone@gmail.com>
2022-09-27 15:13:39 +0530

    Update Table1.sql

--- dbscripts/Tables/Table1.sql
+++ dbscripts/Tables/Table1.sql
@@ -13,3 +13,11 @@ create table demo.Emp2 (
     address varchar(50),
     city varchar(30)
 )
+
+--changeset saradhi.makkena:db3
+create table demo.Emp3 (
+    name varchar(50),
+    address varchar(50),
+    city varchar(30)
+)
+
