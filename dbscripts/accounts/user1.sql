7c6a1ebfa90ff2b419f0b1f91174e54f15ef78fe
pardhuawsone <pardhuawsone@gmail.com>
2022-09-27 15:06:09 +0530

    Update user1.sql

--- dbscripts/accounts/user1.sql
+++ dbscripts/accounts/user1.sql
@@ -2,3 +2,10 @@
 
 --changeset saradhi.makkena:a12
 create role saradhi_tester2;
+
+--changeset saradhi.makkena:a34
+create role test_role1;
+
+--changeset saradhi.makkena:a56
+create user test_user1;
+
