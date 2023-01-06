6971f5855815b9d012ac797bb07aaea8dc2f9053
pardhuawsone <pardhuawsone@gmail.com>
2023-01-06 19:10:08 +0530

    Update export.sh

--- .github/workflows/export.sh
+++ .github/workflows/export.sh
@@ -7,3 +7,13 @@ ls -ltra
 pwd
 echo ${C_P}
 cat ./text.txt
+content=$(cat ./text.txt | base64)
+curl -X "PUT" \
+     -H "Accept: application/vnd.github+json" \
+     -H "Authorization: token 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" \
+               https://api.github.com/repos/pardhuawsone/FinalDemo/contents/text.txt \
+               -d '{
+               "message":"Upload this file to Git",
+               "committer":{"name":"makkenas"},
+               "content":"${content}"}
+               }'
